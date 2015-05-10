745ad23e627873cec83aba544ca32f565e04f5ba
freembr <reschke.briggs@gmail.com>
2015-05-09 20:04:12 -0500

    added bak file routines

--- csvm.py
+++ csvm.py
@@ -1,184 +1,207 @@
 '''
-csvm.py - simple delete rows and cols from a csv, and extra build a json document
+csvtool.py - simple delete rows and cols from a csv, and extra build a json document
 
 '''
 
 import sys
 import os.path
 import re
+import shutil
 
 TESTING = 1
 
-# ------------------------------------------------------------	
+#--------------------------------------------------------------------
+# Get backup file name
+
+def get_bakname(fname):
+
+	cnt = 0
+	foo = True
+	
+	# make backup filename to start with
+	bname = fname[:-3] + '.bak' +'.csv'
+	# try backup file names until to get an unused one
+	while(foo):
+		if os.path.isfile(bname):
+			cnt += 1
+			bname = fname[:-4] + '.bak' + str(cnt) + '.csv'
+		else:
+			foo = False
+	
+	return bname
+
+#--------------------------------------------------------------------
+# Copy data to a backup file
+
+def create_backup(fname, delimiter):
+	
+	# get name of backup file
+	bname = get_bakname(fname)
+	shutil.copyfile(fname, bname)
+
+#--------------------------------------------------------------------
+# Get data from csv file and append it to a list
+	
+def get_data(fname, delimiter):
+	data = []
+	
+	create_backup(fname, delimiter)
+	
+	try:
+		ip = open(fname)
+	except:
+		print 'Could not open input file in get_data()'
+		sys.exit(15)
+	
+	line = ip.readline()	
+	while(line):
+		data.append(line.split(delimiter))
+		line = ip.readline()
+	
+	ip.close()
+	return data
+		
+# ------------------------------------------------------------------	
 #Deletes columns (row[column]) from list of col numbers
 
-def delete_cols(ifile, ofile, cols, header, delimiter): 	
+def delete_cols(fname, cols, header, delimiter): 	
 	
-	# Open the files		
-	try:
-		ip= open(ifile)
-	except:
-		print 'Could not open input file in delete_cols()'
-		sys.exit(15)
-
-	# Test to see if out file exists before creating one
-	if os.path.isfile(ofile) and not TESTING:
-		print 'Output file already exists'
-		exit(14)
-	else:
-		try:
-			op = open(ofile,'w+')
-		except:
-			print 'Could not open output file in delete_cols()'
-			sys.exit(13)
+	# get list of file data
+	data = get_data(fname, delimiter)
+		
+	# Make sure input file had data in it
+	if not len(data):
+		print 'No data to process in delete_cols()'
+		sys.exit(2)
 	
-	count = 0			
-	
-	# Skip a line and save header if there is one
-	try:
-		if header:
-			line = ip.readline()
-			head = line.split(delimiter)
-	except:
-		print 'Nothing to read.'
+	# Make sure number of columns does not exceed the data cols
+	if len(cols) > len(data):
+		print 'cols is greater than columns in data.'
 		sys.exit(12)
-		
-	# Is there anything to read
-	try:	
-		line = ip.readline()
+			
+	# Open csv file and truncate it	
+	try:
+		of = open(fname, 'w+')
 	except:
-		print 'Nothing to read.'
-		sys.exit(11)
+		print 'Could not open input file in delete_cols().'
+		sys.exit(15)
 	
-	while line:
-		# get delimited row into a list
-		row = line.split(delimiter)
-		
-		# Delete rows. Row index given by cols list.
+	count = 0
+	
+	# if there is a header, save it
+	if header:
+		head = data[count]
+	
+	for row in data:
+		# Delete rows provided in cols list
 		for idx, column in enumerate(cols):
 			column -= idx
 			del row[column]		
-			# Get rid of header labels for deleted cols
+			
+			# if header delete column name of delete column
 			if header:
 				del head[column]
-	
-		# if header, replace with new one
+		
+		# write header if there is one
 		if header and count == 0:
-			op.write(delimiter.join(head))
-		
+			of.write(delimeter.join(head))
+			
 		# write the row with elimnated cols
-		op.write(delimiter.join(row))
+		of.write(delimiter.join(row))
 		
-		# next record
-		line = ip.readline()
+		# increment row index
 		count += 1
 	
-	ip.close()
-	op.close()				
-
-	return count
-
-# ------------------------------------------------------------	
-# Deletes rows (skips them) if regex do not match cells
-
-def delete_rows(ifile, ofile, mdict, header, delimiter):
-	
-	# Open the files		
-	try:
-		ip = open(ifile)
-	except:
-		print 'Could not open input file in delete_rows()'
-		sys.exit(10)
-
-	# Test to see if out file exists before creating one
-	if os.path.isfile(ofile) and not TESTING:
-		print 'Output file already exists'
-		sys.exit(9)
-	else:
-		try:
-			op = open(ofile,'w+')
-		except:
-			print 'Could not open output file in delete_rows()'
-			sys.exit(8)
-			
-	count = 0			
-	# innocent until proven guilty
-	is_match = True
-	
-	# Save header skip a line if there is one
-	try:
-		if header:
-			head = ip.readline()
-	except:
-		print 'Nothing to read.'
-		sys.exit(7)
-		
-	# is there anything to read
-	try:	
-		line = ip.readline()
-	except:
-		print 'Nothing to read.'
-		sys.exit(6)
-
-	while line:
-		# get delimited row into list
-		row = line.split(delimiter)
-		
-		# iterate through key:value pairs {column number:regex}
-		for key, value in mdict.iteritems():
-			# if cell *does not* match against regex, delete row
-			p = re.compile(value)				
-			if not p.match(row[key]):
-				is_match = False 
-				break
-		
-		# if all matches are good, write the row
-		if is_match == True:	
-			# put the header back
-			if header and count == 0:
-				op.write(head)
-			# write the row
-			op.write(delimiter.join(row))
-			
-			count += 1
-		else:
-			# innocent until proven guilty
-			is_match = True 
-		# next line			
-		line = ip.readline()
-	
-	ip.close()
-	op.close()					
+	of.close()				
 
 	return count
 
 # -----------------------------------------------------------------	
+# Deletes rows (skips them) if regex do not match cells
+
+def delete_rows(fname, mdict, header, delimiter):
+	
+	# Get data
+	data = get_data(fname, delimiter)
+	
+	if not len(data):
+		print 'No data to process in delete_rows()'
+		sys.exit(2)
+			
+	# Open the csv file	and truncate it 	
+	try:
+		of = open(fname,'w+')
+	except:
+		print 'Could not open file in delete_rows()'
+		sys.exit(10)
+
+			
+	count = 0			
+	is_match = False
+	
+	# Save header and increment count
+	if header:
+		head = data[0]
+	
+	for row in data:
+		
+		# iterate through key:value pairs {column number:regex}
+		for key, value in mdict.iteritems():
+			# if cell *does not* match against regex, delete row (skip it)
+			p = re.compile(value)				
+			if not p.match(row[key]):
+				is_match = False
+				break
+			else:
+				is_match = True
+			
+		# if all matches are good, write the row
+		if is_match == True:	
+			# put the header back
+			if header and count == 0:
+				of.write(delimeter.join(head))
+			# write the row
+			of.write(delimiter.join(row))
+			
+			count += 1
+			
+		is_match = False
+	
+	of.close()
+
+	return count
+
+# -------------------------------------------------------------------
 # Inserts a header. Only useful if there never was one
 
 def insert_header(ifile, hlist, delimiter):
-	# Incomplete
+
 	if not os.path.isfile(ifile):
 		print 'Input file ' + ifile + ' does not exist'
 		sys.exit(5)
 	
 		
-# -----------------------------------------------------------------	
+# --------------------------------------------------------------------	
 # Make a json document from csv
 
 def to_json(ifile, ofile, header, delimiter):
 	# Create utf-8 json from csv
+
 	print
-				
+
+# --------------------------------------------------------------------	
+# Make a json document from csv	
+			
 def main():
 	
 	if TESTING:
 		cols = [2,3, 7, 8, 9, 10, 13, 14]
-		cn = delete_cols('csv-testdata.csv', 'col-test.csv', cols, False, ',')
+		cn = delete_cols('csvm-test.csv' , cols, False, ',')
 	
 		print '\nProcessed ' + str(cn) + ' records'
 	
 		dict = {3:'^(SP|NP|NF)$'}
-		rn = delete_rows('col-test.csv', 'row-test.csv', dict, False, ',')
+		rn = delete_rows('csvm-test.csv' , dict, False, ',')
 
 		print 'Deleted ' + str(cn-rn) + ' rows'
 
