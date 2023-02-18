65466bd3e86c9ce02b3fd7d87e8c315855ac6834
briggsreschke <feature.frame@gmail.com>
2023-02-18 01:55:23 -0600

    Add files via upload

---
+++ csv_to_json.py
@@ -0,0 +1,8 @@
+import csv
+import json
+
+with open('read.csv') as f:
+    a = [{k: v for k, v in row.items()} for row in csv.DictReader(f, skipinitialspace=True)]
+
+with open('write.json', 'w') as fout:
+    json.dump(a, fout)
